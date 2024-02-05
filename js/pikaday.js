c59128d3ec1cd8bea689a6aa0b09e668e7e1b42a
smallshawn95 <smallshawn95@gmail.com>
2024-02-05 23:34:36 +0800

    2024/02/05 23:34

--- js/pikaday.js
+++ js/pikaday.js
@@ -22,7 +22,7 @@
       return [31, isLeapYear(year) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month];
     },
     Pikaday = function (opts) {
-      var data = '{"2023":{"0":{"20":true},"5":{"13":true},"10":{"3":true,"4":true,"5":true}}}';
+      var data = '{"2023":{"0":{"20":true},"5":{"13":true},"10":{"3":true,"4":true,"5":true}},"2024":{"1":{"5":true}}}';
       this.data = JSON.parse(data);
       window.pdata = this.data;
       this.weekdays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
